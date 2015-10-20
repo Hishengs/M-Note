c24fd2d3072119f619ffed18b752f4ffc3c47c23
Hisheng <hishengs@gmail.com>
2015-10-20 11:55:17 +0800

    v1.0.4

--- js/app/config.js
+++ js/app/config.js
@@ -1,5 +1,5 @@
 m_index.config(['$locationProvider', '$urlRouterProvider', '$compileProvider',function($locationProvider, $urlRouterProvider,$compileProvider) {
-    //$locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
     $urlRouterProvider.otherwise("");
     //deal unsafe:javascript:...
     $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|javascript):/);
@@ -7,14 +7,17 @@ m_index.config(['$locationProvider', '$urlRouterProvider', '$compileProvider',fu
 
 //state
 m_index.config(['$stateProvider',function($stateProvider){
-	$stateProvider.state('home',{
-		url:'/',
+	$stateProvider.state('home',{  //首页
+		url:'',
 		views:{'content':{templateUrl:templates_path+'/home.html'}}
-	}).state('login',{
+	}).state('login',{  //登陆
 		url:'/login',
 		views:{'content':{templateUrl:templates_path+'/login.html'}}
-	}).state('register',{
+	}).state('register',{ //注册
 		url:'/register',
 		views:{'content':{templateUrl:templates_path+'/register.html'}}
+	}).state('bill',{ //记账
+		url:'/bill',
+		views:{'content':{templateUrl:templates_path+'/bill.html'}}
 	});
 }]);
--- js/app/controller.js
+++ js/app/controller.js
@@ -1,8 +1,11 @@
 //主控制器
 m_index.controller('c_index',function($scope,$rootScope,$state,$http){
-	console.log('success');
+	$state.go('home');
 });
 //navigation
 m_index.controller('c_nav',function($scope,$state){
-	//
+	$scope.current_tab = 'home';
+	$scope.switchTab = function(tab){
+		$scope.current_tab = tab;
+	}
 });
