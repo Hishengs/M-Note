ca5162bda407c9fb54b4b72dd437aa22502223f8
Hisheng <hishengs@gmail.com>
2015-10-25 15:56:58 +0800

    v1.3.0

--- js/app/config.js
+++ js/app/config.js
@@ -16,9 +16,25 @@ m_index.config(['$stateProvider',function($stateProvider){
 	}).state('register',{ //注册
 		url:'/register',
 		views:{'content':{templateUrl:templates_path+'/register.html'}}
-	}).state('bill',{ //记账
+	}).state('bill',{ //--------------------------------------记账-----------------------------------------------
 		url:'/bill',
 		views:{'content':{templateUrl:templates_path+'/bill/bill.html'}}
+	}).state('bill_details',{ //记账-账单详情
+		url:'/bill_details',
+		parent:'bill',
+		views:{'bill_details_view':{templateUrl:templates_path+'/bill/bill_details.html'}}
+	}).state('modify_bill',{ //记账-账单修改
+		url:'/modify_bill',
+		parent:'bill',
+		views:{'bill_details_view':{templateUrl:templates_path+'/bill/modify_bill.html'}}
+	}).state('modify_bill_outcome',{ //记账-账单修改-outcome
+		url:'/modify_bill_outcome',
+		parent:'modify_bill',
+		views:{'modify_bill_view':{templateUrl:templates_path+'/bill/modify_bill_outcome.html'}}
+	}).state('modify_bill_income',{ //记账-账单修改-income
+		url:'/modify_bill_income',
+		parent:'modify_bill',
+		views:{'modify_bill_view':{templateUrl:templates_path+'/bill/modify_bill_income.html'}}
 	}).state('bill_outcome',{ //支出
 		url:'/outcome',
 		parent:'bill',
@@ -27,14 +43,14 @@ m_index.config(['$stateProvider',function($stateProvider){
 		url:'/income',
 		parent:'bill',
 		views:{'bill_view':{templateUrl:templates_path+'/bill/income.html'}}
-	}).state('charts',{ //报表
+	}).state('charts',{ //------------------------------------报表----------------------------------------------------
 		url:'/charts',
 		views:{'content':{templateUrl:templates_path+'/charts/charts.html'}}
 	}).state('budget',{ //报表-预算
 		url:'/budget',
 		parent:'charts',
 		views:{'charts_view':{templateUrl:templates_path+'/charts/budget.html'}}
-	}).state('accounts',{ //账户
+	}).state('accounts',{ //----------------------------------账户-----------------------------------------------------
 		url:'/accounts',
 		views:{'content':{templateUrl:templates_path+'/accounts/accounts.html'}}
 	}).state('cash',{ //账户-现金
--- js/app/controller.js
+++ js/app/controller.js
@@ -1,3 +1,4 @@
+var controller_path = '';
 //主控制器
 m_index.controller('c_index',function($scope,$rootScope,$state,$http){
 	$state.go('home');
@@ -9,12 +10,29 @@ m_index.controller('c_nav',function($scope,$state){
 		$scope.current_tab = tab;
 	}
 });
+//----------------------------------------记账页面----------------------------------------------
+//|-+c_bill
+//|---c_add_bill_modal
+//|---+c_bill_details_modal
+//|---|---c_modify_bill
+
 //c_bill
-m_index.controller('c_bill',function($scope,$state){
+m_index.controller('c_bill',function($scope,$state,$http){
+	//获取当日账单列表，bill_list
+	/*$http.get(controller_path+'').success(function(res){
+		if(res.error === 0)
+			$scope.bill_list = res.data;
+			$scope.bill_tip_show = false;
+		else if(res.error === 2){//查询为空
+			$scope.bill_tip_show = true;
+			$scope.bill_tip = '今天还没有账单信息，快去记一笔吧！';
+		}
+		else console.log(res.msg);
+	});*/
 	$scope.bill_tip_show = true;
 	$scope.bill_tip = '今天还没有账单信息，快去记一笔吧！';
-	$scope.showBillModal = function(){
-		var bill_modal = UIkit.modal("#new-bill-modal");
+	$scope.showAddBillModal = function(){//显示新增账单模态框
+		var bill_modal = UIkit.modal("#add-bill-modal");
 		if ( bill_modal.isActive() ) {
 		    bill_modal.hide();
 		} else {
@@ -22,18 +40,18 @@ m_index.controller('c_bill',function($scope,$state){
 		}
 		$state.go('bill_outcome');
 	}
-	$scope.showBillDetail = function(){
-		var bill_detail_modal = UIkit.modal("#bill-detail");
+	$scope.showBillDetail = function(){//显示账单详情模态框
+		var bill_detail_modal = UIkit.modal("#bill-detail-modal");
 		if ( bill_detail_modal.isActive() ) {
 		    bill_detail_modal.hide();
 		} else {
 		    bill_detail_modal.show();
 		}
-		//$state.go('bill_outcome');
+		$state.go('bill_details');
 	}
 });
-//c_bill_modal
-m_index.controller('c_bill_modal',function($scope,$state){
+//c_add_bill_modal
+m_index.controller('c_add_bill_modal',function($scope,$state){
 	$scope.bill_type = '记账-支出';
 	$scope.current_bill_view = 'outcome';
 	var date = new Date();
@@ -50,7 +68,44 @@ m_index.controller('c_bill_modal',function($scope,$state){
 		if($scope.current_bill_view == 'outcome'){$state.go('bill_income');$scope.current_bill_view = 'income';$scope.bill_type = '记账-收入';}
 		else {$state.go('bill_outcome');$scope.current_bill_view = 'outcome';$scope.bill_type = '记账-支出';}
 	}
+	//账单post
 });
+//c_bill_details_modal
+m_index.controller('c_bill_details_modal',function($scope,$state,$http){
+	$scope.deleteBill = function(){//删除账单
+		confirm('你确定要删除该账单？本操作不可撤销！');
+	}
+	$scope.modifyBill = function(){//修改账单
+		$state.go('modify_bill');
+	}
+});
+//c_modify_bill
+m_index.controller('c_modify_bill',function($scope,$state,$http){
+	$scope.current_bill_type = 'outcome';
+	$state.go('modify_bill_outcome');
+	$scope.bill_modify_tip_show = false;//提示
+	//根据账单id获取账单类型，根据账单类型对应显示不同的view(outcome/income)
+	$scope.switchBillType = function(){//切换支出-收入
+		if($scope.current_bill_type == 'outcome'){
+			$state.go('modify_bill_income');
+			$scope.current_bill_type = 'income';
+		}else{
+			$state.go('modify_bill_outcome');
+			$scope.current_bill_type = 'outcome';
+		}
+		$scope.bill_modify_tip_show = true;
+		$scope.bill_modify_tip = '切换类型后账单<b>类别</b>可能需要修改！';
+	}
+	$scope.backward = function(){//返回账单详情页面
+		$scope.bill_modify_tip_show = false;
+		$state.go('bill_details');
+	}
+});
+
+//----------------------------------------报表页面----------------------------------------------
+//---+c_charts
+//---|---c_budget
+
 //c_charts
 m_index.controller('c_charts',function($scope,$state){
 	$state.go('budget');
@@ -61,6 +116,11 @@ m_index.controller('c_charts',function($scope,$state){
 });
 //c_budget
 m_index.controller('c_budget',function($scope,$state){});
+
+//----------------------------------------账户页面----------------------------------------------
+//---+c_accounts
+//---|---c_cash
+
 //c_accounts
 m_index.controller('c_accounts',function($scope,$state){
 	$state.go('cash');
